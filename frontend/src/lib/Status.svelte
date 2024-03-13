aae3f7e780c944cab69e28cf0e28c2d8bb2e5263
Yudai Udagawa <nimius.rd@gmail.com>
2024-03-13 15:04:50 +0000

    Update

---
+++ frontend/src/lib/Status.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+  const socket = getContext("socket");
+  console.log("socket", socket);
+</script>
+
+<p>Status: {$socket.readyState}</p>
