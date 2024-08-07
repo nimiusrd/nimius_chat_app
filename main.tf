1a1500efa443fe19ed7adf83e9352bbcec07e9af
Yudai Udagawa <nimius.rd@gmail.com>
2024-08-07 19:30:31 +0000

    Add terraform

---
+++ main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configures the provider to use the resource block's specified project for quota checks.
+provider "google-beta" {
+  user_project_override = true
+}
+
+# Configures the provider to not use the resource block's specified project for quota checks.
+# This provider should only be used during project creation and initializing services.
+provider "google-beta" {
+  alias                 = "no_user_project_override"
+  user_project_override = false
+}
+
+# Creates a new Google Cloud project.
+resource "google_project" "default" {
+  provider = google-beta.no_user_project_override
+
+  name       = var.project_name
+  project_id = var.project_id
+  # Required for any service that requires the Blaze pricing plan
+  # (like Firebase Authentication with GCIP)
+  billing_account = var.billing_account
+
+  # Required for the project to display in any list of Firebase projects.
+  labels = {
+    "firebase" = "enabled"
+  }
+}
+
+# Enables required APIs.
+resource "google_project_service" "default" {
+  provider = google-beta.no_user_project_override
+  project  = google_project.default.project_id
+  for_each = toset([
+    "cloudbilling.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "firebase.googleapis.com",
+    # Enabling the ServiceUsage API allows the new project to be quota checked from now on.
+    "serviceusage.googleapis.com",
+    # Enabling the Text-to-Speech API for the new project.
+    "texttospeech.googleapis.com",
+  ])
+  service = each.key
+
+  # Don't disable the service if the resource block is removed by accident.
+  disable_on_destroy = false
+}
+
+# Enables Firebase services for the new project created above.
+resource "google_firebase_project" "default" {
+  provider = google-beta
+  project  = google_project.default.project_id
+
+  # Waits for the required APIs to be enabled.
+  depends_on = [
+    google_project_service.default
+  ]
+}
+
+resource "google_firebase_web_app" "default" {
+  provider     = google-beta
+  project      = google_project.default.project_id
+  display_name = var.firebase_app_name
+}
+
+data "google_firebase_web_app_config" "default" {
+  provider   = google-beta
+  project    = google_project.default.project_id
+  web_app_id = google_firebase_web_app.default.app_id
+}
+
+resource "google_firebase_hosting_site" "default" {
+  provider = google-beta.no_user_project_override
+  project  = google_project.default.project_id
+  site_id  = var.firebase_app_site_id
+  app_id   = google_firebase_web_app.default.app_id
+}
--- variables.tf
+++ variables.tf
@@ -10,6 +10,7 @@ variable "project_id" {
 
 variable "billing_account" {
   type        = string
+  sensitive   = true
   description = "The billing account to associate with the project"
 }
 
@@ -23,4 +24,19 @@ variable "zone" {
   default     = "asia-northeast1-a"
   type        = string
   description = "The zone to deploy resources"
+}
+
+variable "firebase_app_name" {
+  type        = string
+  description = "The display name of the Firebase app"
+}
+
+variable "firebase_bucket_name" {
+  type        = string
+  description = "The name of the Firebase storage bucket"
+}
+
+variable "firebase_app_site_id" {
+  type        = string
+  description = "The ID of the Firebase app's site"
 }
