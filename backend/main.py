5c8dd9dd2ee689b369b87424665b2729fae40a58
Yudai Udagawa <nimius.rd@gmail.com>
2024-08-08 17:21:52 +0000

    Update websocket server address to listen on all available interfaces

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ async def handler(websocket: WebSocketServerProtocol) -> None:
 
 
 async def main():
-    async with websockets.serve(handler, "localhost", 8001):
+    async with websockets.serve(handler, "0.0.0.0", 8001):
         await asyncio.Future()
 
 
