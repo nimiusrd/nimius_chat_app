19e106f7da7b50c5ad638d8e33a548ef07ae0e95
Yudai Udagawa <nimius.rd@gmail.com>
2024-08-14 04:01:32 +0000

    Fix method

--- backend/speech_synthesizer/base.py
+++ backend/speech_synthesizer/base.py
@@ -1,9 +1,9 @@
+from fastapi.websockets import WebSocket
 from logger import speech_synthesizer_logger
-from websockets import WebSocketServerProtocol
 
 
 class Speaker:
-    async def talk(self, text: str, websocket: WebSocketServerProtocol, is_bot: bool = False) -> None:
+    async def talk(self, text: str, websocket: WebSocket, is_bot: bool = False) -> None:
         speech_synthesizer_logger.info("Talking to '%s'", text)
 
         if (content := self._synthesize(text, is_bot)) is None:
@@ -11,11 +11,11 @@ class Speaker:
             return
         if is_bot:
             try:
-                await websocket.send(text)
+                await websocket.send_text(text)
             except Exception as e:
                 speech_synthesizer_logger.error("Failed to send text to websocket. %s", e)
         try:
-            await websocket.send(content)
+            await websocket.send_bytes(content)
         except Exception as e:
             speech_synthesizer_logger.error("Failed to send audio to websocket. %s", e)
         speech_synthesizer_logger.info("Successfully talked to '%s'", text)
