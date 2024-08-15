bc72413b37766555d5099f520c4afeff57f45dae
Yudai Udagawa <nimius.rd@gmail.com>
2024-08-15 09:19:47 +0000

    Add Terraform Cloud Build trigger for automatic Terraform apply

--- main.tf
+++ main.tf
@@ -46,6 +46,7 @@ resource "google_project_service" "default" {
     "secretmanager.googleapis.com",
     "cloudbuild.googleapis.com",
     "run.googleapis.com",
+    "cloudkms.googleapis.com",
     # Enabling the ServiceUsage API allows the new project to be quota checked from now on.
     "serviceusage.googleapis.com",
     # Enabling the Text-to-Speech API for the new project.
@@ -55,6 +56,45 @@ resource "google_project_service" "default" {
 
   # Don't disable the service if the resource block is removed by accident.
   disable_on_destroy = false
+}
+
+resource "google_kms_key_ring" "tfstate_key_ring" {
+  project  = google_project.default.project_id
+  name     = "tfstate-key-ring"
+  location = var.region
+}
+
+resource "google_kms_crypto_key" "tfstate_crypto_key" {
+  name            = "tfstate-crypto-key"
+  key_ring        = google_kms_key_ring.tfstate_key_ring.id
+  rotation_period = "100000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+data "google_storage_project_service_account" "gcs_account" {
+  project = google_project.default.project_id
+}
+
+resource "google_kms_crypto_key_iam_binding" "binding" {
+  crypto_key_id = google_kms_crypto_key.tfstate_crypto_key.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+}
+
+resource "google_storage_bucket" "tfstate_bucket" {
+  project  = google_project.default.project_id
+  name     = "${var.project_id}-tfstate"
+  location = var.region
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.tfstate_crypto_key.id
+  }
+
+  depends_on = [google_kms_crypto_key_iam_binding.binding]
 }
 
 # Enables Firebase services for the new project created above.
@@ -310,34 +350,4 @@ resource "google_cloudbuild_trigger" "run-terraform-plan" {
 
   filename        = "cloudbuild.terraform-plan.yaml"
   service_account = google_service_account.cloud_build_sa.id
-}
-
-output "config" {
-  value = {
-    projectId         = google_firebase_project.default.project
-    appId             = google_firebase_web_app.default.app_id
-    apiKey            = data.google_firebase_web_app_config.default.api_key
-    authDomain        = data.google_firebase_web_app_config.default.auth_domain
-    storageBucket     = lookup(data.google_firebase_web_app_config.default, "storage_bucket", "")
-    messagingSenderId = lookup(data.google_firebase_web_app_config.default, "messaging_sender_id", "")
-    measurementId     = lookup(data.google_firebase_web_app_config.default, "measurement_id", "")
-  }
-}
-
-output "need-to-set-secret" {
-  description = "Set the secret in the Secret Manager."
-  value = {
-    url                 = "https://console.cloud.google.com/security/secret-manager?project=${google_project.default.project_id}"
-    backend_config_toml = google_secret_manager_secret.backend_config_toml.id
-    frontend_dotenv     = google_secret_manager_secret.frontend_dotenv.id
-    terraform_tfvars    = google_secret_manager_secret.terraform_tfvars.id
-  }
-}
-
-output "service-account" {
-  description = "Service account for Artifact Registry"
-  value = {
-    artifact_registry = google_service_account.artifact_registry_sa.email
-    cloud_build       = google_service_account.cloud_build_sa.email
-  }
 }
